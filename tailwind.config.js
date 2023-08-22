a9110dc1f50207ef6042c6229751c13fd8c0e08e
kauankarvalho <kauan.karvalho@outlook.com>
2023-08-22 17:04:06 -0300

    feat: add font, images, and logo to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./src/pages/*.html"],
   theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
