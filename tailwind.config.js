286be3e23fc2d83f415570f3efbabfeded0b9324
kauankarvalho <kauan.karvalho@outlook.com>
2023-08-24 16:45:57 -0300

    feat: add animations to pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
     fontFamily: {
       roboto: ["Roboto", "sans-serif"],
     },
+
+    keyframes: {
+      slideDown: {
+        "0%": { transform: "translateY(-50px)", opacity: "0" },
+        "100%": { transform: "translateY(0)", opacity: "1" },
+      },
+
+      slideLeft: {
+        "0%": { transform: "translateX(100px)", opacity: "0" },
+        "100%": { transform: "translateX(0)", opacity: "1" },
+      },
+    },
+
+    animation: {
+      slideDown: "slideDown 1.5s ease",
+      slideLeft: "slideLeft 1.5s ease",
+    },
+
     extend: {},
   },
   plugins: [],
