1fa8477d44e3522887ccd50d28469c4db8890cb8
kauankarvalho <kauan.karvalho@outlook.com>
2023-08-23 22:03:02 -0300

    refactor: Object-Oriented design

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,11 @@
-const routes = {
-  "/": "./src/pages/home.html",
-  "/universe": "./src/pages/universe.html",
-  "/exploration": "./src/pages/exploration.html",
-}
+import { Router } from "./router.js"
 
-function loadRouteHTML() {
-  const app = document.querySelector("#app")
-  const route = routes[location.pathname]
+const router = new Router()
 
-  fetch(route)
-    .then((data) => data.text())
-    .then((html) => {
-      app.innerHTML = html
-      addClickHandlers()
-    })
-}
+router.add("/", "./src/pages/home.html")
+router.add("/universe", "./src/pages/universe.html")
+router.add("/exploration", "./src/pages/exploration.html")
 
-function addClickHandlers() {
-  const home = document.querySelector("#nav-home")
-  const universe = document.querySelector("#nav-universe")
-  const exploration = document.querySelector("#nav-exploration")
+router.loadRouteHTML()
 
-  home.addEventListener("click", handleNavigationClick)
-  universe.addEventListener("click", handleNavigationClick)
-  exploration.addEventListener("click", handleNavigationClick)
-}
-
-function handleNavigationClick(event) {
-  event.preventDefault()
-  history.pushState({}, "", event.target.href)
-  loadRouteHTML()
-}
-
-loadRouteHTML()
+window.onpopstate = () => router.loadRouteHTML()
---
+++ src/js/router.js
@@ -0,0 +1,39 @@
+export class Router {
+  constructor() {
+    this.routes = {}
+    this.handleNavigationClick = this.handleNavigationClick.bind(this)
+  }
+
+  add(routerName, page) {
+    this.routes[routerName] = page
+  }
+
+  handleNavigationClick(event) {
+    event.preventDefault()
+    history.pushState({}, "", event.target.href)
+
+    this.loadRouteHTML()
+  }
+
+  loadRouteHTML() {
+    const app = document.querySelector("#app")
+    const route = this.routes[location.pathname]
+
+    fetch(route)
+      .then((data) => data.text())
+      .then((html) => {
+        app.innerHTML = html
+        this.addClickHandlers()
+      })
+  }
+
+  addClickHandlers() {
+    const home = document.querySelector("#nav-home")
+    const universe = document.querySelector("#nav-universe")
+    const exploration = document.querySelector("#nav-exploration")
+
+    home.addEventListener("click", this.handleNavigationClick)
+    universe.addEventListener("click", this.handleNavigationClick)
+    exploration.addEventListener("click", this.handleNavigationClick)
+  }
+}
